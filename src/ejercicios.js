bf3d1a76c62d6bb4100ff5ab87a73952a5e515e8
bylev <2009014@upy.edu.mx>
2025-11-16 19:40:52 -0600

    feat: Ejercicio 4.1 resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -447,19 +447,25 @@ function rotar90Grados(matriz) {
 function mezclarImagenes(matriz1, matriz2, factor) {
   // TODO: Implementar mezcla de imágenes
   
-  // 1. Verificar que tengan las mismas dimensiones
-  // const dims1 = obtenerDimensiones(matriz1);
-  // const dims2 = obtenerDimensiones(matriz2);
-  // if (dims1.filas !== dims2.filas || dims1.columnas !== dims2.columnas) {
-  //   throw new Error('Las imágenes deben tener el mismo tamaño');
-  // }
-  
-  // 2. Para cada pixel:
-  // r = r1 * (1 - factor) + r2 * factor
-  // g = g1 * (1 - factor) + g2 * factor
-  // b = b1 * (1 - factor) + b2 * factor
-  
-  return []; // REEMPLAZAR
+  // Crea una nueva matriz para el resultado
+  const resultado = crearMatrizVacia(matriz1.length, matriz1[0].length);
+
+  // Recorre cada pixel y mezcla los colores
+  for (let i = 0; i < matriz1.length; i++) {
+    // Recorre cada columna
+    for (let j = 0; j < matriz1[i].length; j++) {
+      const pixel1 = matriz1[i][j];
+      const pixel2 = matriz2[i][j];
+      // Mezcla cada canal usando el factor 
+      resultado[i][j] = {
+        r: limitarValorColor(Math.round(pixel1.r * (1 - factor) + pixel2.r * factor)),
+        g: limitarValorColor(Math.round(pixel1.g * (1 - factor) + pixel2.g * factor)),
+        b: limitarValorColor(Math.round(pixel1.b * (1 - factor) + pixel2.b * factor)),
+        a: limitarValorColor(Math.round(pixel1.a * (1 - factor) + pixel2.a * factor))
+      };
+    }
+  }
+  return resultado; // Devuelve la matriz resultante
 }
 
 /**
